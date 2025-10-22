4c181e770bf35eed46248ee594c2b2bdd9a8d253
Shivanshu08Verma <shivanshuv2005@gmail.com>
2025-10-22 16:56:10 +0530

    routes and controllers

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(
@@ -12,6 +12,14 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-application.use(cookieParser());
+app.use(cookieParser());
+
+
+//routes
+import userRouter from "./routes/user.routes.js"
+
+//routes  declaration
+app.use("/api/v1/users",userRouter)
+
 
 export { app };
